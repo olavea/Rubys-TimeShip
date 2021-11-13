cb1f71cab0661ea7f0a4b647685c4cf2743778cb
Ola <ola@olavea.com>
2021-11-13 13:17:06 +0100

    fix: deleted api/pirates-pizza-sourcing-in-sanity.js

--- src/api/pirates-pizza-sourcing-in-sanity.js
+++
@@ -1,8 +0,0 @@
-const sanity = require('sanity')(process.env.SANITY_TOKEN);
-
-export default async function piratePizzaInSanity( req, res ) {
-    res.status(200).json({
-        message: `I Copy`,
-    });
-
-}
