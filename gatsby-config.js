d88da814a5594d20e5271c890e34c6807602c4e5
Ola <ola@olavea.com>
2021-11-25 10:49:00 +0100

    fix: something is wrong 😬

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,9 @@ module.exports = {
     siteUrl: "https://demo.raae.codes",
     title: "@raae demo starter",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud", "@raae/gatsby-plugin-new-css"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "@raae/gatsby-plugin-new-css",
+    `@raae/gatsby-plugin-donations`
+  ],
 };
