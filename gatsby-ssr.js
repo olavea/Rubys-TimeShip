792553880fdc92202615e989e70efa08d11ae715
Ola <ola@olavea.com>
2021-12-02 12:48:08 +0100

    fix: monitorDom: true #12

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,12 +3,14 @@ import React from "react";
 const OutsetaScriptComponent = () => {
     const o_options = {
         domain: 'timeship.outseta.com',
+        monitorDom: true
       };
     return (
         <script
             key="outseta-script"
             src="https://cdn.outseta.com/outseta.min.js"
             data-options={o_options}
+
         ></script>
     );
 };
