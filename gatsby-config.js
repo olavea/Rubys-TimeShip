b84611427f968232f99d2f12757b31f23016a05b
Ola <ola@olavea.com>
2022-01-05 06:57:42 +0100

    fix: @netlify/plugin-gatsby@^2.0.0-beta + netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,55 @@ module.exports = {
   siteMetadata: {
     siteUrl: "https://timeship1.gatsbyjs.io/",
     title: `TimeShip1`,
-    description: "Lillian and Friends Building Stuff",
+    description: "Lillian and Friends Building Skill",
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
+    'gatsby-plugin-netlify',
     "@raae/gatsby-plugin-new-css",
     `gatsby-plugin-sharp`,
+//  Currently you cannot use StaticImage
+//  or gatsby-transformer-sharp in SSR or DSG pages. The best workaround is to use an image CDN such as Cloudinary or imgix to host your images. This will give you faster builds and rendering too.
     `gatsby-transformer-sharp`,
     `gatsby-transformer-remark`,
+
+    // {
+    //   resolve: `gatsby-transformer-remark`,
+    //   options: {
+    //     plugins: [
+    //       {
+    //         resolve: `gatsby-remark-prismjs`,
+    //       },
+    //       {
+    //         resolve: `gatsby-remark-images`,
+    //         options: {
+    //           maxWidth: 800,
+    //           linkImagesToOriginal: false,
+    //         },
+    //       },
+    //       {
+    //         resolve: `@raae/gatsby-remark-oembed`,
+    //         options: {
+    //           providers: {
+    //             // Important to exclude providers
+    //             // that adds js to the page.
+    //             // If you do not need them.
+    //             exclude: [
+    //               "Reddit",
+    //               "Instagram",
+    //               "Twitter",
+    //               "Flickr",
+    //               "YouTube",
+    //             ],
+    //           },
+    //         },
+    //       },
+    //       {
+    //         resolve: `gatsby-remark-responsive-iframe`,
+    //       },
+    //     ],
+    //   },
+    // },
 
     {
       resolve: `gatsby-source-filesystem`,
@@ -31,6 +72,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-image`,
+    `gatsby-plugin-react-helmet`,
     `@raae/gatsby-plugin-donations`,
     // `@raae/gatsby-plugin-let-it-snow`,
     {
