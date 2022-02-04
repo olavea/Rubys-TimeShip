034558cc400409e0f6ee1ae6ecd026dffa875c6f
Ola <ola@olavea.com>
2022-02-04 14:48:07 +0100

    fix: does it work in prod? OlaCast 15 #10

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
 
 export default function Footer() {
   return (
     <footer>
-      <p>&copy; Lillian and Friends and Family Building Skill in {new Date().getFullYear()}</p>
+      <p>
+        &copy; Lillian (7{" "}
+        <span role="img" aria-label="pirate princess emojis">
+          🏴‍☠️👸
+        </span>
+        ) and Cap'n Ola Vea (43{" "}
+        <span role="img" aria-label="pirate ship emojis">
+          🏴‍☠️⛵
+        </span>
+        ) and their cheerful crew of skill-builder pirates 1554 -{" "}
+        {new Date().getFullYear()}
+      </p>
     </footer>
   );
 }
